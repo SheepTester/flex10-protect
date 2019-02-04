86fe5cf1c06abf61e6da877e36648e232a15ed49
SheepTester <ussa.say@gmail.com>
2019-02-03 21:00:03 -0800

    player, dumb shooting

--- main.js
+++ main.js
@@ -19,23 +19,87 @@ function genDiacriticBeam(width = 4, length = 100) {
   return str;
 }
 
+function compareDistance(dx, dy, dist) {
+  const squares = dx * dx + dy * dy;
+  const square = dist * dist;
+  return squares > square ? 1 : squares < square ? -1 : 0;
+}
+
 const GAME_WIDTH = 300; // 1:1.5
 const GAME_HEIGHT = 450;
+const GAME_WIDTH_RADIUS = GAME_WIDTH / 2;
 const NAVBAR_HEIGHT = 60;
+const controls = { movement: {strength: 0, direction: 0}, shooting: {direction: 0, active: false} };
+let gameWindow = {y: NAVBAR_HEIGHT};
 function doScreenSizeCalculations() {
   const width = window.innerWidth;
   const height = window.innerHeight - NAVBAR_HEIGHT;
   if (height / width > 1.5) {
-    document.body.style.setProperty('--scale', width / GAME_WIDTH);
+    document.body.style.setProperty('--scale', gameWindow.scale = width / GAME_WIDTH);
     document.body.style.setProperty('--right', 0);
-    // document.body.style.setProperty('--width', '100%');
+    0;
   } else {
-    document.body.style.setProperty('--scale', height / GAME_HEIGHT);
+    document.body.style.setProperty('--scale', gameWindow.scale = height / GAME_HEIGHT);
     document.body.style.setProperty('--right', (width - GAME_WIDTH * height / GAME_HEIGHT) / 2 + 'px');
-    // document.body.style.setProperty('--width', GAME_WIDTH * height / GAME_HEIGHT + 'px');
   }
+  gameWindow.x = width / 2;
 }
 window.addEventListener('resize', doScreenSizeCalculations);
+
+function getMouseXY(pair) {
+  return {
+    mouseX: (pair.clientX - gameWindow.x) / gameWindow.scale,
+    mouseY: (pair.clientY - gameWindow.y) / gameWindow.scale
+  };
+}
+document.addEventListener('mousemove', e => {
+  const {mouseX, mouseY} = getMouseXY(e);
+  controls.shooting.direction = Math.atan2(mouseY - player.y, mouseX - player.x);
+});
+document.addEventListener('mousedown', e => {
+  controls.shooting.active = true;
+});
+document.addEventListener('mouseup', e => {
+  controls.shooting.active = false;
+});
+
+const keys = {left: false, up: false, right: false, down: false};
+function keyControls() {
+  controls.movement.strength = 1;
+  if (keys.up && !keys.down) {
+    if (keys.left && !keys.right) controls.movement.direction = 5 * Math.PI / 4;
+    else if (keys.right && !keys.left) controls.movement.direction = 7 * Math.PI / 4;
+    else controls.movement.direction = 3 * Math.PI / 2;
+  } else if (keys.down && !keys.up) {
+    if (keys.left && !keys.right) controls.movement.direction = 3 * Math.PI / 4;
+    else if (keys.right && !keys.left) controls.movement.direction = Math.PI / 4;
+    else controls.movement.direction = Math.PI / 2;
+  } else {
+    if (keys.left && !keys.right) controls.movement.direction = Math.PI;
+    else if (keys.right && !keys.left) controls.movement.direction = 0;
+    else controls.movement.strength = 0;
+  }
+}
+document.addEventListener('keydown', e => {
+  switch (e.keyCode) {
+    case 37: case 65: keys.left = true; break;
+    case 38: case 87: keys.up = true; break;
+    case 39: case 68: keys.right = true; break;
+    case 40: case 83: keys.down = true; break;
+    case 32: controls.shooting.active = true; break;
+  }
+  keyControls();
+});
+document.addEventListener('keyup', e => {
+  switch (e.keyCode) {
+    case 37: case 65: keys.left = false; break;
+    case 38: case 87: keys.up = false; break;
+    case 39: case 68: keys.right = false; break;
+    case 40: case 83: keys.down = false; break;
+    case 32: controls.shooting.active = false; break;
+  }
+  keyControls();
+});
 
 const objects = [];
 
@@ -49,6 +113,7 @@ class Positionable {
 
     this.elem = document.createElement('div');
     this.elem.classList.add('floating');
+    this.isPositionable = true;
   }
 
   tick(timePassed) {
@@ -69,6 +134,8 @@ class Positionable {
 
 }
 
+const POST_WIDTH_RADIUS = 100;
+const POST_HEIGHT_RADIUS = 50;
 class Post extends Positionable {
 
   constructor(author, content) {
@@ -78,6 +145,8 @@ class Post extends Positionable {
     this.elem.textContent = content;
 
     this.prog = 0;
+    this.bulletCollideable = true;
+    this.isPost = true;
   }
 
   tick(timePassed) {
@@ -85,28 +154,119 @@ class Post extends Positionable {
     this.y = GAME_HEIGHT - this.prog * GAME_HEIGHT / 100;
   }
 
+  onhit() {
+    console.log('post hit!');
+  }
+
 }
 
+const bulletTypes = {
+  player: {speed: 200, type: ''},
+  paffy: {speed: 10, type: 'diacritic'},
+  poof: {speed: 10, type: 'diacritic'}
+};
+const BULLET_RADIUS = 2.5;
 class Bullet extends Positionable {
 
-  constructor(initX, initY, direction, speed, type = '') {
+  constructor(initX, initY, parent, direction, speed, type = '') {
     super(initX, initY);
     this.elem.classList.add('bullet');
-    this.elem.classList.add('bullet-' + type);
+    if (type) this.elem.classList.add('bullet-' + type);
     this.dx = Math.cos(direction);
     this.dy = Math.sin(direction);
     this.speed = speed;
+    this.parent = parent;
+    this.isBullet = true;
   }
 
   tick(elapsedTime) {
     this.x += this.dx * this.speed * elapsedTime / 1000;
     this.y += this.dy * this.speed * elapsedTime / 1000;
-    if (this.x > screenWidthRadius || this.x < -screenWidthRadius || this.y < 0 || this.y > screenHeight) {
+    if (this.x > GAME_WIDTH_RADIUS || this.x < -GAME_WIDTH_RADIUS || this.y < 0 || this.y > GAME_HEIGHT) {
       this.remove();
+    } else {
+      objects.find(obj => {
+        if (obj.bulletCollideable && obj !== this.parent) {
+          if (obj.isIndividual) {
+            if (compareDistance(this.x - obj.x, this.y - obj.y, INDIVIDUAL_RADIUS + BULLET_RADIUS) <= 0) {
+              this.remove();
+              obj.onhit();
+              return true;
+            }
+          } else if (obj.isPost) {
+            if (this.x - BULLET_RADIUS < obj.x + POST_WIDTH_RADIUS && this.x + BULLET_RADIUS > obj.x - POST_WIDTH_RADIUS
+                && this.y - BULLET_RADIUS < obj.y + POST_HEIGHT_RADIUS && this.y + BULLET_RADIUS > obj.y - POST_HEIGHT_RADIUS) {
+              this.remove();
+              obj.onhit();
+              return true;
+            }
+          }
+        }
+      });
     }
   }
 
 }
+
+const INDIVIDUAL_RADIUS = 15;
+class Individual extends Positionable {
+
+  constructor(initX, initY, person) {
+    super(initX, initY);
+    this.elem.classList.add('person');
+    this.elem.classList.add('person-' + person);
+    this.direction = 0;
+    this.speed = 0;
+    this.xv = this.yv = 0;
+
+    this.person = person
+    this.bulletCollideable = true;
+    this.isIndividual = true;
+  }
+
+  tick(elapsedTime) {
+    this.xv += (Math.cos(this.direction) * this.speed - this.xv) * 0.6 * elapsedTime / 30;
+    this.yv += (Math.sin(this.direction) * this.speed - this.yv) * 0.6 * elapsedTime / 30;
+    this.x += this.xv;
+    this.y += this.yv;
+  }
+
+  shoot(direction = this.direction) {
+    this.elem.parentNode.appendChild(new Bullet(this.x, this.y, this, direction, bulletTypes[this.person].speed, bulletTypes[this.person].type).elem);
+  }
+
+  onhit() {
+    console.log('individual hit!');
+  }
+
+}
+
+const SHOOTING_SPEED = 200;
+class Player extends Individual {
+
+  constructor() {
+    super(0, INDIVIDUAL_RADIUS * 2, 'player');
+    this.shootingTimer = 0;
+  }
+
+  tick(elapsedTime) {
+    this.direction = controls.movement.direction;
+    this.speed = controls.movement.strength * 5;
+    if (controls.shooting.active) {
+      this.shootingTimer -= elapsedTime;
+      if (this.shootingTimer < 0) {
+        this.shootingTimer = SHOOTING_SPEED;
+        this.shoot(controls.shooting.direction);
+      }
+    } else {
+      this.shootingTimer = 0;
+    }
+    super.tick(elapsedTime);
+  }
+
+}
+
+const player = new Player();
 
 let lastTime = Date.now();
 function tick() {
@@ -126,5 +286,12 @@ document.addEventListener('DOMContentLoaded', e => {
   gameWrapper = document.getElementById('game');
   const post = new Post('munkler', 'All students are now required to recite the SELF anthem every morning during the announcements.');
   gameWrapper.appendChild(post.elem);
+  setTimeout(() => {
+    const post = new Post('gggta', 'ربما سأكون الراسم أنا بالفعل T racer ماذا عن ويدماكر؟ أنا بالفعل Widow market سأكون باستيون نيرف باستيون أنت على حق. لذا ، ونستون أريد أن أكون وينستون أعتقد أنني سأكون جينجي أنا بالفعل جينجي ثم سأكون مكري لقد اخترت بالفعل مكري 🇸🇦🇸🇦🇸🇦🇸🇦🇸🇦');
+    gameWrapper.appendChild(post.elem);
+  }, 12000);
+  gameWrapper.appendChild(new Individual(-40, 160, 'paffy').elem);
+  gameWrapper.appendChild(new Individual(40, 160, 'poof').elem);
+  gameWrapper.appendChild(player.elem);
   tick();
 }, {once: true});
